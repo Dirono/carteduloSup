7e9f0e26bbbf1d4280b6a81e64ef32f0bd654ecc
dirono <droom.official.i@gmail.com>
2018-06-14 17:28:51 -0400

    asd
    asd

--- data.js
+++ data.js
@@ -1,22 +1,4 @@
 parsko([{
-<<<<<<< HEAD
-=======
-	"classID": 1299,
-	"sectionNumber": "",
-	"courseCode": "AP",
-	"className": " Activity Period",
-	"teacherName": "AP",
-	"teacherFirstName": "AP",
-	"teacherLastName": "AP",
-	"type": "none",
-	"allottedTimes": [
-		["T", 1230, 1400],
-		["H", 1230, 1400]
-	],
-	"teacherRating": "none",
-	"teacherNumberRatings": "none"
-}, {
->>>>>>> 389548f316f264d9e18cd9dcefa8abb382b86456
 	"classID": 1,
 	"sectionNumber": "00001",
 	"courseCode": "101-LCU-05",
@@ -19397,7 +19379,6 @@ parsko([{
 	],
 	"teacherRating": "none",
 	"teacherNumberRatings": "none"
-<<<<<<< HEAD
 }, {
 	"classID": 1299,
 	"sectionNumber": "",
@@ -19414,6 +19395,3 @@ parsko([{
 	"teacherRating": "none",
 	"teacherNumberRatings": "none"
 }]);
-=======
-}]);
->>>>>>> 389548f316f264d9e18cd9dcefa8abb382b86456
